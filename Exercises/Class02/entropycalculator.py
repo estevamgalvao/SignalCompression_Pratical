7134353ecfd14dce24bf48cdd84ec000de3fca04
Estevam Albuquerque <33361102+estevamgalvao@users.noreply.github.com>
2021-02-10 12:00:09 +0000

    first try to develop order entropy
    something is not working so well due to the entropy level isn't decreasing. i'll figure out what's is wrong

--- Exercises/Class02/entropycalculator.py
+++ Exercises/Class02/entropycalculator.py
@@ -1,20 +1,31 @@
 import collections
 import math
 
-def entropyCalc(input, order = 1):
+def entropyCalc(sequel, order = 1):
     H = 0
-    if order == 1:
-        freq = collections.Counter(input)
-        ocurrences = sum(freq.values())
-        
-        for key in freq.keys():
-            #H = sum(-P(x)*log(P(x)))
-            prob = freq[key]/ocurrences
-            H += -(prob*math.log2(prob))
-        return H
+    sequel = [(sequel[i:i+order]) for i in range(0, len(sequel), order)]
+    #print(sequel)
+
+    freq = collections.Counter(sequel)
+    print(freq)
+    ocurrences = sum(freq.values())
+    
+    for key in freq.keys():
+        #H = sum(-P(x)*log(P(x)))
+        prob = freq[key]/ocurrences
+        H += -(prob*math.log2(prob))
+    return H
 
 
-input = input()
-if input == '':
-    input = [1, 2, 3, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10]
-print(entropyCalc(input))
+user_input = input("Sequel: ")
+order = int(input("Order: "))
+
+
+if user_input == '':
+    user_input = "ATGCTTAAGCTGCTTAACCTGAAGCTTCCGCTGAAGAACCTGCTGAACCCGCTTAAGCTGAACCTTCTGAAGCTTAACCTGCTT"
+
+#print(entropyCalc(user_input, order))
+print(entropyCalc(user_input, 1))
+print(entropyCalc(user_input, 2))
+print(entropyCalc(user_input, 3))
+print(entropyCalc(user_input, 4))
