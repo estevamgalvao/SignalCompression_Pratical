2b677b09f06c1c86b22946af7ac6aa4cb104494d
Estevam Albuquerque <33361102+estevamgalvao@users.noreply.github.com>
2021-02-21 16:32:04 +0000

    refining

--- Exercises/Class02/entropycalculator.py
+++ Exercises/Class02/entropycalculator.py
@@ -3,15 +3,19 @@ import math
 
 def entropyCalc(sequel, order = 1):
     G = 0
+    
+    # building the list of symbols inside the sequel
     sequel = [(sequel[i:i+order]) for i in range(0, len(sequel), order)]
-    #print(sequel)
-
+    
     freq = collections.Counter(sequel)
-    #print(freq)
     ocurrences = sum(freq.values())
     
+    # plan to calculate the total of information in this sequel using the
+    # following expression:
+    # G == sum(-P(x)*log2(P(x)))
+
+    # every key in this collection is a sequel's symbol
     for key in freq.keys():
-        #G == sum(-P(x)*log(P(x)))
         prob = freq[key]/ocurrences
         G += -(prob*math.log2(prob))
     return G/order
@@ -20,12 +24,9 @@ def entropyCalc(sequel, order = 1):
 user_input = input("Sequel: ")
 order = int(input("Order: "))
 
-
+# facilitating debug/tests
 if user_input == '':
     user_input = "ATGCTTAAGCTGCTTAACCTGAAGCTTCCGCTGAAGAACCTGCTGAACCCGCTTAAGCTGAACCTTCTGAAGCTTAACCTGCTT"
 
-#print(entropyCalc(user_input, order))
-print(entropyCalc(user_input, 1))
-print(entropyCalc(user_input, 2))
-print(entropyCalc(user_input, 3))
-print(entropyCalc(user_input, 4))
+
+print("\nH(s): ", entropyCalc(user_input, order))
