42baf98de6627bf070ebd1d3b10f074630abc32b
Estevam Albuquerque <33361102+estevamgalvao@users.noreply.github.com>
2021-02-21 23:59:56 +0000

    Create test.cpp

---
+++ Huffman Project/src/test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+
+int main(int argc, char const *argv[])
+{
+    std::map<char,int> freq;
+    std::vector<std::tuple> lol;
+
+
+    /*
+    freq['a'] = 4;
+    freq['b'] = 1;
+    freq['c'] = 4;
+    freq['d'] = 1;
+    freq['e'] = 2;
+    freq['f'] = 4;
+    */
+
+   
+
+    
+    return 0;
+}
+
+//aabaacdceecfcfff
