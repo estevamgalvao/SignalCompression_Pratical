a3106227f85b6f5835a450f7c35760b5246c5a5a
Estevam Albuquerque <33361102+estevamgalvao@users.noreply.github.com>
2021-03-14 10:45:23 +0000

    sort compare function fixed
    it seems that the sort function doesn't pass pointers to the compare func, so i changed this:
    "Node *n1 --> Node n1", Hence
    "n1->weight_ --> n1.weight" same with n2 as well

--- Huffman Project/src/main.cpp
+++ Huffman Project/src/main.cpp
@@ -1,22 +1,24 @@
 #include "CodeTree.h"
 
-bool CompareNodeByWeight (Node *n1, Node *n2) {
-    return n1->weight_ < n2->weight_;
+bool CompareNodeByWeight (Node n1, Node n2) {
+    return n1.weight_ > n2.weight_;
 }
 
 
 int main(int argc, char const *argv[])
 {
-    
+    /*
     char arr[] = {'b', 'o', 'o', 'k'};
     int n = sizeof(arr) / sizeof(arr[0]);
  
     std::vector<char> vect(arr, arr + n);
     std::vector<char>::iterator itr_vec;
+    */
 
     std::unordered_map<char, unsigned int> dict;
     std::unordered_map<char, unsigned int>::iterator itr_dict; 
 
+    // Filling the dictionary
     dict['b'] = 1;
     dict['o'] = 2;
     dict['k'] = 1;
@@ -24,10 +26,8 @@ int main(int argc, char const *argv[])
     std::vector<Node> node_vector;
     std::vector<Node>::iterator itr_node_vec;
 
-    itr_dict = dict.begin();
-
-
     Node *aux_n;
+    Node b = Node::Node('j', (u4)2);
 
     for (itr_dict = dict.begin()++; itr_dict != dict.end(); ++itr_dict) {
         aux_n = new Node(itr_dict->first, itr_dict->second);
@@ -40,7 +40,16 @@ int main(int argc, char const *argv[])
         << itr_node_vec->weight_;
     }
 
-    std::cout << "End";
+    std::sort(node_vector.begin(), node_vector.end(), &CompareNodeByWeight);
+    std::cout << "\n\nSORTED";
+    
+    for (itr_node_vec = node_vector.begin(); itr_node_vec != node_vector.end();
+    ++itr_node_vec) {
+        std::cout << "\nSymbol: " << itr_node_vec->GetSymbol() << "\t Weight: "
+        << itr_node_vec->weight_;
+    }
+
+    
 
     return 0;
 }
