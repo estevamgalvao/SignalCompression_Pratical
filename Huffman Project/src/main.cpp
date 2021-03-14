698d6f3e1a48b94196f8d35867c676459fbec0ea
Estevam Albuquerque <33361102+estevamgalvao@users.noreply.github.com>
2021-03-14 12:22:44 +0000

    some updates
    - why when i add the aux node to the vector, the node get recursive left and right nodes

--- Huffman Project/src/main.cpp
+++ Huffman Project/src/main.cpp
@@ -26,11 +26,8 @@ int main(int argc, char const *argv[])
     std::vector<Node> node_vector;
     std::vector<Node>::iterator itr_node_vec;
 
-    Node *aux_n;
-    Node b = Node::Node('j', (u4)2);
-
     for (itr_dict = dict.begin()++; itr_dict != dict.end(); ++itr_dict) {
-        aux_n = new Node(itr_dict->first, itr_dict->second);
+        Node *aux_n = new Node(itr_dict->first, itr_dict->second);
         node_vector.push_back(*aux_n);
     }
 
@@ -49,7 +46,35 @@ int main(int argc, char const *argv[])
         << itr_node_vec->weight_;
     }
 
-    
+    while (node_vector.size() > 1) {
+/*         std::vector<Node>::iterator itr1;
+        std::vector<Node>::iterator itr2; */
+        Node *aux_n;
+        unsigned int s = node_vector.size();
+
+        Node *n1 = &node_vector[s-2];
+        Node *n2 = &node_vector[s-1];
+
+        aux_n = new Node(NULL,
+                        n1->weight_ + n2->weight_,
+                        n1,
+                        n2);
+        aux_n->SetLeaf(false);
+
+        node_vector.pop_back();
+        node_vector.pop_back();
+        node_vector.push_back(*aux_n);
+        std::sort(node_vector.begin(), node_vector.end(), &CompareNodeByWeight);
+    }
+
+    itr_node_vec = node_vector.begin();
+    std::cout << "Nó: " << itr_node_vec->GetRightNode()->GetSymbol();
+
+    for (itr_node_vec = node_vector.begin(); itr_node_vec != node_vector.end();
+    ++itr_node_vec) {
+        std::cout << "\nSymbol: " << itr_node_vec->GetSymbol() << "\t Weight: "
+        << itr_node_vec->weight_;
+    }
 
     return 0;
 }
