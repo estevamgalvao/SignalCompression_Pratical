d7f9289d340f7653c5fac86e837a1c6f80400a29
estevamgalvao <estevamgalvao1@gmail.com>
2021-03-18 19:35:29 +0000

    working gtest - path not 100%

---
+++ Exercises/GTest/src/mainTest.cpp
@@ -0,0 +1,10 @@
+#include "sqrt.h"
+#include <gtest/gtest.h>
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << squareRoot(123.0) << "\n";
+    squareRoot(31.1);
+    return 0;
+}
